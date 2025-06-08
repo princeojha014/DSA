fb131fd471743c0ff2eda2fe1eb0f1e4896591a3
Prince Ojha <princeojha014@gmail.com>
2025-06-08 22:05:05 +0530

    Main

--- Howework/Day 9/2_star.cpp
+++ Howework/Day 9/2_star.cpp
@@ -1,8 +1,22 @@
-
+/*
+A
+A B
+A B C
+A B C D
+A B C D E
+*/
 
 #include<iostream>
 using namespace std;
 int main()
 {
-
+    for(int i=1; i<=5; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            char ch='A'+ j-1;
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
 }
--- Howework/Day 9/4_star.cpp
+++ Howework/Day 9/4_star.cpp
@@ -1,8 +1,22 @@
-
+/*
+A B C D
+A B C
+A B
+A
+*/
 
 #include<iostream>
 using namespace std;
 int main()
 {
+    for(int i=4; i>=1; i--)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            char ch= 'A' +  j-1;
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
 
 }
