d01d7d89141c76c72dcca1221b5f677a86b4f3e0
Prince Ojha <princeojha014@gmail.com>
2025-06-11 15:48:23 +0530

    Main

---
+++ Practice/L10_Operators/1_BinaryUnary.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Binary Opertor;
+    cout<<13/2<<endl;           //6
+    cout<<5.2*2<<endl;          //10.4
+    cout<<4*13.2<<endl;         //52.8
+    
+    // Unary Operator
+    int a=10;
+    a++;                       // 10+1=11
+    cout<<a<<endl;
+
+    int b=10;
+    ++a;                       // 1+10=11
+    cout<<a<<endl;
+
+}
---
+++ Practice/L10_Operators/2_ComparisonOperators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Comparison Operators (0/1)
+
+    int a,b;
+    cout<<"Enter The Value Of A: ";
+    cin>>a;
+    cout<<"Enter The Value Of B: ";
+    cin>>b;
+
+    // == 
+    if(a==b)
+    cout<<"Yes"<<endl;
+    else
+    {
+        cout<<"No"<<endl;
+    }
+}
